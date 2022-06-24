690d2c5db38e5157eb5eaa612741c37678da873e
Souravgoswami <souravgoswami@protonmail.com>
2022-06-25 03:29:42 +0530

    Refactor and shebang line

--- Prompt-2 MRuby/PromptRb/ruby_to_c_char*_converter.rb
+++ Prompt-2 MRuby/PromptRb/ruby_to_c_char*_converter.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env -S ruby --disable-gems
+
 data = IO.foreach(File.join(__dir__, 'bash_prompt.rb')).reject { |x|
 	stripped = x.lstrip
 	stripped.empty? || stripped.start_with?(?#)
@@ -9,6 +11,5 @@ data.gsub!(?\r, "\\r")
 data.gsub!('"', '\"')
 
 c_code = data.lines.map { |slice| %Q(\t"#{slice.strip}\\n") }.join(?\n)
-c_code
 
 puts %Q(char *code() { return(\n#{c_code}\n);})
