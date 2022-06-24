3588e0f264f1886032a0ed14e62ad4c12de8f623
Souravgoswami <souravgoswami@protonmail.com>
2022-06-25 03:06:19 +0530

    Converter generates C code directly

--- Prompt-2 MRuby/PromptRb/ruby_to_c_char*_converter.rb
+++ Prompt-2 MRuby/PromptRb/ruby_to_c_char*_converter.rb
@@ -1,8 +1,14 @@
-data = IO.read(File.join(__dir__, 'bash_prompt.rb'))
+data = IO.foreach(File.join(__dir__, 'bash_prompt.rb')).reject { |x|
+	stripped = x.lstrip
+	stripped.empty? || stripped.start_with?(?#)
+}.join
 
 data.gsub!(/(\\.{1})/) { |x| "\\#{x}" }
-data.gsub!(?\t, "\\t")
+data.gsub!(?\t, '')
 data.gsub!(?\r, "\\r")
 data.gsub!('"', '\"')
 
-puts data.lines.map { |slice| %Q(\t"#{slice.strip}\\n") }.join(?\n)
+c_code = data.lines.map { |slice| %Q(\t"#{slice.strip}\\n") }.join(?\n)
+c_code
+
+puts %Q(char *code() { return(\n#{c_code}\n);})
